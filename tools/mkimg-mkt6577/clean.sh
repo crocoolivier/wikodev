ad80b43608e08625d652855995f7e215194adb84
crocoolivier <crocodanser@gmail.com>
2013-03-07 14:54:29 +0100

    first code commit

---
+++ tools/mkimg-mkt6577/clean.sh
@@ -0,0 +1,3 @@
+make TARGET_PRODUCT=s9081 MTK_ROOT_CUSTOM=../mediatek/custom CROSS_COMPILE=/home/olivier/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- clean mrproper
+rm -rf boot.img
+rm -rf kernelflasher.zip
