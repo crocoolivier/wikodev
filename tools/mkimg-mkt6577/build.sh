49fad2da2f3e2587e64759c9d714f69124612e66
crocoolivier <crocodanser@gmail.com>
2013-03-07 17:50:18 +0100

    delete

--- tools/mkimg-mkt6577/build.sh
+++
@@ -1,5 +0,0 @@
-make TARGET_PRODUCT=s9081 MTK_ROOT_CUSTOM=../mediatek/custom CROSS_COMPILE=/home/olivier/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-echo -e "Making boot.img"
-./repack.pl -boot arch/arm/boot/zImage ramdisk boot.img
-#echo -e "Making flashable zip"
-#zip -r kernelflasher META-INF boot.img
